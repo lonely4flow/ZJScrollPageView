af53514c571f28e20928bf3932321d336ae724e8
独孤流 <47106210+lonely4flow@users.noreply.github.com>
2019-08-15 15:16:51 +0800

    设置配合FDFullGesture框架侧滑可返回的功能

--- ZJScrollPageView/ZJScrollPageView/ZJCollectionView.m
+++ ZJScrollPageView/ZJScrollPageView/ZJCollectionView.m
@@ -27,5 +27,13 @@
 - (void)setupScrollViewShouldBeginPanGestureHandler:(ZJScrollViewShouldBeginPanGestureHandler)gestureBeginHandler {
     _gestureBeginHandler = [gestureBeginHandler copy];
 }
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+    if (self.contentOffset.x <= 0) {
+        if ([otherGestureRecognizer.delegate isKindOfClass:NSClassFromString(@"_FDFullscreenPopGestureRecognizerDelegate")]) {
+            return YES;
+        }
+    }
+    return NO;
+}
 
 @end
